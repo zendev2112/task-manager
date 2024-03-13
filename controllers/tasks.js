0a84dec34db3a777722617a177d929ae4a32de60
zen <zenon.deviagge@gmail.com>
2024-03-13 16:05:43 -0300

    Refactor getAllTasks controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task = require('../models/tasks');
 
-const getAllTasks = (req, res) => {
-  res.send('get all tasks');
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
