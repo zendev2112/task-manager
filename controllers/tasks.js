0bac9d68ac0a0ba5f433ecf8da2037a8795d0dea
zen <zenon.deviagge@gmail.com>
2024-03-14 17:12:32 -0300

    Refactor updateTask controller.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -31,10 +31,6 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send('update task');
-};
-
 const deleteTask = async (req, res) => {
   try {
     const { id: taskId } = req.params;
@@ -47,6 +43,25 @@ const deleteTask = async (req, res) => {
     res.status(500).json({ msg: error });
   }
 };
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
 module.exports = {
   getAllTasks,
   createTask,
