1864370125de59718b8cea36e675140c3a9901da
zen <zenon.deviagge@gmail.com>
2024-03-15 15:57:27 -0300

    Add notFound middleware with custom error message.

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
-
+const notFound = require('./middleware/not-found');
 // middleware
+app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
-app.get('/hello', (req, res) => {
-  res.send('Task Manager App');
-});
-
 app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
 
 const port = 3000;
 
